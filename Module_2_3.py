f68e17f62f7f1e849baa72970a6ee6358b31cc64
DmitryKolychev <kodm82@mail.ru>
2024-05-29 11:02:56 +0300

    Update Module_2_3.py
    Добавил список

--- Module_2_3.py
+++ Module_2_3.py
@@ -1,11 +1,15 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
+my_list_new = ["a", "b"]
 while a <= (len(my_list)-1):
     number = my_list[a]
     if number > 0:
         print(number)
         a = a + 1
-        continue
+        my_list_new.append(number)
     else:
         a = a + 1
 print('Числовой ряд закончился')
+my_list_new.remove("a")
+my_list_new.remove("b")
+print(my_list_new)
