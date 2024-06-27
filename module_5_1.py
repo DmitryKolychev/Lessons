4538026a3e9a9b4c9c81646a80b5908c25897e58
DmitryKolychev <kodm82@mail.ru>
2024-06-27 15:17:22 +0300

    Update module_5_1.py
    Исправил функцию go_to

--- module_5_1.py
+++ module_5_1.py
@@ -3,8 +3,9 @@ class House:
         self.name = name
         self.number_of_floors = floor
 
-    def go_to(self):
-        new_floor = int(input(f"Какой этаж Вас интересует в {self.name}? "))
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        self.new_floor = int(input(f"Какой этаж Вас интересует в {self.name}? "))
         if 1 <= new_floor <= self.number_of_floors:
             for i in range(new_floor):
                 print(i + 1)
@@ -16,7 +17,7 @@ house_1 = House('Vidnoe', 5)
 house_2 = House('Pavshino', 16)
 print(house_1.name, house_1.number_of_floors)
 print(house_2.name, house_2.number_of_floors)
-house_1.go_to()
-house_2.go_to()
+house_1.go_to(5)
+house_2.go_to(10)
 house_3 = House(input(f'Введите название ЖК: '), int(input(f'Введите количество этажей: ')))
-house_3.go_to()
+house_3.go_to(0)
