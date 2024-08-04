df8727c7392db675039a4de35bf06b22b54d2b75
DmitryKolychev <kodm82@mail.ru>
2024-08-04 07:54:49 +0300

    Итераторы

---
+++ module_9_5.py
@@ -0,0 +1,63 @@
+class StepValueError(ValueError):
+    pass
+
+
+class Iterator:
+
+    def __init__(self, start, stop, step=1):
+        self.start = start
+        self.stop = stop
+        self.step = step
+        if self.step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        if ((self.step > 0 and self.pointer <= self.stop)
+                or (self.step < 0 and self.pointer >= self.stop)):
+            result = self.pointer
+            self.pointer += self.step
+            return result
+        raise StopIteration
+
+
+try:
+    iter1 = Iterator(100, 200, 10)
+    for i in iter1:
+        print(i, end=' ')
+    print()
+except StepValueError as err:
+    print(err)
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+try:
+    for i in iter2:
+        print(i, end=' ')
+    print()
+except StepValueError as err:
+    print(err)
+try:
+    for i in iter3:
+        print(i, end=' ')
+    print()
+except StepValueError as err:
+    print(err)
+try:
+    for i in iter4:
+        print(i, end=' ')
+    print()
+except StepValueError as err:
+    print(err)
+try:
+    for i in iter5:
+        print(i, end=' ')
+    print()
+except StepValueError as err:
+    print(err)
